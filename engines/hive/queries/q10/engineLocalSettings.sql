ca733132d8caa72dee9cd9303bfa6f7c0914dd17
kellyzly <kellyzly@126.com>
2017-07-06 16:48:17 +0800

    why we need disable hive.optimize.sampling.orderby in q10 because of a hive bug, can you give the detail jira number?

--- engines/hive/queries/q10/engineLocalSettings.sql
+++ engines/hive/queries/q10/engineLocalSettings.sql
@@ -4,7 +4,7 @@
 --The source code contained or described herein and all documents related to the source code ("Material") are owned by Intel Corporation or its suppliers or licensors. Title to the Material remains with Intel Corporation or its suppliers and licensors. The Material contains trade secrets and proprietary and confidential information of Intel or its suppliers and licensors. The Material is protected by worldwide copyright and trade secret laws and treaty provisions. No part of the Material may be used, copied, reproduced, modified, published, uploaded, posted, transmitted, distributed, or disclosed in any way without Intel's prior express written permission.
 --
 --No license under any patent, copyright, trade secret or other intellectual property right is granted to or conferred upon you by disclosure or delivery of the Materials, either expressly, by implication, inducement, estoppel or otherwise. Any license under such intellectual property rights must be express and approved by Intel in writing.
-
+why we need disable hive.optimize.sampling.orderby in q10 because of a hive bug, can you give the detail jira number?
 --If your query will not run with  hive.optimize.sampling.orderby=true because of a hive bug => disable orderby sampling locally in this file. 
 --Default behaviour is to use whatever is default in ../engines/hive/conf/engineSettings.sql
 --No need to disable orderby sampling globally for all queries and no need to modifiy the query files q??.sql.
